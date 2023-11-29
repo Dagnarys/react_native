61775c652e36788f9d8f22239b4431284578c21b
dagnarys <super.iard@yandex.ru>
2023-11-29 19:41:35 +0300

    start

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, StatusBar,Text, Image, View } from 'react-native';
+import styled from 'styled-components/native';
 
+const Driver = styled.View`
+    position: relative;
+    display: flex;
+    margin: 10px;
+    padding: 10px;
+    margin-top:30px;
+    border: 2px solid red;
+    max-height: 100%;
+    width: 395px;
+
+    border-radius: 10px; /* Увеличил радиус закругления углов */
+    /*background-color: rgb(206, 170, 170);*/
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); /* Добавил тень для карточек */
+    transition: transform 0.2s ease-in-out; /* Анимация при наведении курсора */
+`;
+const DriverImage = styled.Image`
+    width:150px;
+    height:150px;
+    border-radius:12px;
+    margin-right:12px;
+
+`;
+const TextName = styled.Text`
+    font-size:16px;
+    font-weight:700;
+`;
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+        <Driver>
+        <DriverImage source = {{ url:'https://i.stack.imgur.com/cXbga.png'}}/>
+        <TextName>Asd</TextName>
+        </Driver>
     </View>
   );
 }
---
+++ components/DriverCard.jsx
@@ -0,0 +1,6 @@
+
+
+<Driver>
+        <DriverImage source = {{ url:'https://i.stack.imgur.com/cXbga.png'}}/>
+        <TextName>Asd</TextName>
+</Driver>
